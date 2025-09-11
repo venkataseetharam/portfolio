0bed9902ef7c8c081e60a5775bb6688ca3e71b20
venkataseetharam <47640996+venkataseetharam@users.noreply.github.com>
2025-09-11 14:45:41 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/portfolio/',
+  base: '/your-web-canvas-70/',
   server: {
     host: "::",
     port: 8080,
